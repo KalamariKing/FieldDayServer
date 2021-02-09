fd0b2a4cedfc7a1b1cab9093f10542a619a4ca56
Max Garner <49308171+KalamariKing@users.noreply.github.com>
2021-02-09 10:51:08 -0500

    Update FieldDayServer.cpp
    Fix strncmp, pointers are still invalid

--- FieldDayServer.cpp
+++ FieldDayServer.cpp
@@ -9,81 +9,78 @@ NOTES TO SELF
 
 #include "Headers.h"
 
-sf::TcpListener listener;
-sf::Packet packet;
+// sf::TcpListener listener;
+// sf::Packet packet;
 
-sf::TcpSocket socket;
+// sf::TcpSocket socket;
 
 advisory advisory1, advisory2, advisory3, advisory4, advisory5, advisory6, advisory7, advisory8, advisory9, advisory10, advisory11, advisory12, advisory13, advisory14, advisory15, advisory16, advisory17, advisory18, advisory19, advisory20, advisory21, advisory22, advisory23, advisory24, advisory25, advisory26, advisory27, advisory28, advisory29, advisory30;
 advisory* curr;
 
-string id, action, output;
+string id, output;
+char action[2] = "0";
 int inValue, value;
 
 int main()
 {
   while (true)
   {
-    if (listener.listen(53000) != sf::Socket::Done)
-    {
-      cout << "Error attempting socket hook on port 53000" << endl;
-    }
+    // if (listener.listen(53000) != sf::Socket::Done)
+    // {
+    //   cout << "Error attempting socket hook on port 53000" << endl;
+    // }
 
-    if (listener.accept(socket) != sf::Socket::Done)
-    {
-      cout << "Error accepting socket" << endl;
-    }
+    // if (listener.accept(socket) != sf::Socket::Done)
+    // {
+    //   cout << "Error accepting socket" << endl;
+    // }
 
-    socket.receive(packet);
+    // socket.receive(packet);
     
-    if (packet >> id >> action >> value)
-    {
-      // success
-    } else {
-      cout << "Error reading: too much data" << endl;
-    }
+    // if (packet >> id >> action >> value)
+    // {
+    //   // success
+    // } else {
+    //   cout << "Error reading: too much data" << endl;
+    // }
 
-    if (strcmp(action, "1") == 0)  // Task
+    if (strncmp(action, "1", 2) == 0)  // Task
     {
       // Copy this line for each advisory
-      if (id == "Teacher1")  { *curr = &advisory1   }
-      if (id == "Teacher2")  { *curr = &advisory2;  }
-      if (id == "Teacher3")  { *curr = &advisory3;  }
-      // ...
-      if (id == "Teacher30") { *curr = &advisory30; }
+      if (id == "Griffin") { *curr = &advisory1; }
       
       // Leave these lines once at the bottom
-      *curr.points += value;
-      cout << "Added " << value << " points to " << *curr.name << "'s grade " << *curr.grade << " team." << endl;
+      *curr->points += value;
+      cout << "Added " << value << " points to " << *curr->name << "'s grade " << *curr->grade << " team." << endl;
     }
 
     else if (strcmp(action, "2") == 0)  // Check Scores
     {
-      socket.send(
-        // Grade 5 advisories:
-        advisory1.points +
-        advisory2.points +
-        advisory3.points +
-        advisory4.points <<
+      // socket.send(
+      //   // Grade 5 advisories:
+      //   advisory1.points +
+      //   advisory2.points +
+      //   advisory3.points +
+      //   advisory4.points <<
         
-        // Grade 6 advisories:
-        advisory1.points +
-        advisory2.points +
-        advisory3.points +
-        advisory4.points <<
+      //   // Grade 6 advisories:
+      //   advisory1.points +
+      //   advisory2.points +
+      //   advisory3.points +
+      //   advisory4.points <<
         
-        // Grade 7 advisories:
-        advisory1.points +
-        advisory2.points +
-        advisory3.points +
-        advisory4.points <<
+      //   // Grade 7 advisories:
+      //   advisory1.points +
+      //   advisory2.points +
+      //   advisory3.points +
+      //   advisory4.points <<
         
-        // Grade 8 advisories:
-        advisory1.points +
-        advisory2.points +
-        advisory3.points +
-        advisory4.points <<
-      );
+      //   // Grade 8 advisories:
+      //   advisory1.points +
+      //   advisory2.points +
+      //   advisory3.points +
+      //   advisory4.points <<
+      // );
     }
     else {
       cout << "Error reading: illegal action, action=" << action << ", required 1 or 2" << endl;
